e6c9c5766dbaba4e5b2ff6dfccd600d7419cba15
felipe <[an-felipe@hotmail.es]>
2022-04-27 14:05:22 +0200

    first commit

---
+++ intro2-app.js
@@ -0,0 +1,25 @@
+import  Button from './Button'
+
+const arr = [
+    'chanchito Feliz',
+    'chanchito triste',
+    'chanchito emocionado'
+]
+const App = ()=> {
+    const miVariable = false;
+    if(miVariable){
+        return <p>Mi variable dio true</p>
+    }
+    return (
+        <div>
+            <h1 onClick={(e)=> console.log('click', e)}>Hola mundo</h1>
+            {arr.map(el=> <p key={el}>{el}</p>)}
+        <Button onClick={()=> console.log('clickeado')}>
+            Enviar
+        </Button>
+        </div>
+    )
+}
+
+
+export default App
---
+++ intro4-app.js
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+
+class Button extends Component{
+    render(){
+        console.log('ejecutando metodo render de button');
+        return(
+            <button>
+                Enviar
+            </button>
+        )
+    }
+}
+
+class App extends Component{
+    state = {
+        valor: 3
+    }
+    render(){
+        console.log(this.state);
+        return (
+            <div>
+                <p>Hola Mundo</p>
+                <Button/>
+                <button 
+                className={`${this.state.valor}`}
+                onClick={ ()=>this.setState({valor: 2})}>
+                    Enviar
+                </button>
+            </div>
+        )
+    }
+}
